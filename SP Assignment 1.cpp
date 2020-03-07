795962fb7635fce696c26cefc8b76d3783d84dfd
MohammedBahaa2320 <55110792+MohammedBahaa2320@users.noreply.github.com>
2020-03-07 12:26:26 +0200

    Add files via upload

---
+++ SP Assignment 1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int i, j;
+	float base_price[3];
+	float taxes[3];
+	float max_price[3];
+	float net_price[3];
+	for(i = 0; i >= 2; i++)
+		cout << "Enter base price of product : ";
+		cin >> base_price[i]; cout << endl;
+		cout << "taxes of the product : ";
+		cin >> taxes[i]; cout << endl;
+		cout << "maximum price of the belonging production line : ";
+		cin >> max_price[i]; cout << endl;
+		net_price[i] = base_price[i] + base_price[i] * taxes[i] / 100; 
+		if (net_price[i] > max_price[i])
+	    cout << "product net price is NOT accepted";
+		else if (net_price[i] < max_price[i])
+		cout << "product net price is accepted";
+}
